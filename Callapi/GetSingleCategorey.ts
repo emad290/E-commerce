61ae8fd140faf6fb347698761cb78f67c88e99c1
emad290 <emadrasmy26@gmail.com>
2025-09-18 14:51:36 +0300

    fix: third commet

--- Callapi/GetSingleCategorey.ts
+++ Callapi/GetSingleCategorey.ts
@@ -1,4 +1,4 @@
-export default async function GetSingleCat(id){
+export default async function GetSingleCat(id:string){
     const res =await fetch(`https://ecommerce.routemisr.com/api/v1/categories/${id}`);
     const data = await res.json();
     return data
