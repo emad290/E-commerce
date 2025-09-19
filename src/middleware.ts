5e0da59bb97bee56d689e816bdac5f7ac2f71374
emad290 <emadrasmy26@gmail.com>
2025-09-19 19:01:18 +0300

    last

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
+import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-    const token =request.cookies.get("next-auth.session-token");
-   
+export async function middleware(request: NextRequest) {
+    // const token =request.cookies.get("next-auth.session-token");
+   const token = await getToken({req:request,secret:process.env.AUTH_SECRET})
     if(!token){
         return NextResponse.redirect(new URL('/Login', request.url))
     }
